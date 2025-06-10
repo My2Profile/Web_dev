e2dd52a8fba72e2fb62a05f9664acfb6093a409c
My2Profile <timkameiramkhan48@gmail.com>
2025-06-10 07:27:57 -0700

    Assignment 2

--- book_catalog_project/book_catalog_project/urls.py
+++ book_catalog_project/book_catalog_project/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from books import urls as books_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(books_urls)),
+    path('', include('books.urls')),
 ]
--- book_catalog_project/books/models.py
+++ book_catalog_project/books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+
+	name = models.CharField(max_length=100, unique=True)
+	description = models.TextField(blank=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+
+
+class Book(models.Model):
+
+	title = models.CharField(max_length=200)
+	author = models.CharField(max_length=150)
+	publication_year = models.IntegerField()
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+
